cac263e5e69fea69970330c27415151c997d0716
unknown <skropp@8KQ5H02.medassurant.local>
2015-09-21 15:39:35 -0400

    materal ui

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,35 @@
-'use strict';
+/*
+  gulpfile.js
+  ===========
+  Rather than manage one giant configuration file responsible
+  for creating multiple tasks, each task has been broken out into
+  its own file in gulp/tasks. Any files in that directory get
+  automatically required below.
+  To add a new task, simply add a new task file that directory.
+  gulp/tasks/default.js specifies the default set of tasks to run
+  when you run `gulp`.
+*/
 
-var gulp = require('gulp');
-var ts = require('gulp-typescript');
+var requireDir = require('require-dir');
 
-var tsProject = ts.createProject('src/tsconfig.json', {
-    typescript: require('typescript')
-});
+// Require all tasks in gulp/tasks, including subfolders
+requireDir('./gulp/tasks', { recurse: true });
 
-// compile-ts
-gulp.task('compile-ts', function () {
-    var tsResult = gulp.src('src/**/*')
-        .pipe(ts(tsProject));
 
-    return tsResult.js.pipe(gulp.dest('build'));
-});
+
+// 'use strict';
+//
+// var gulp = require('gulp');
+// var ts = require('gulp-typescript');
+//
+// var tsProject = ts.createProject('src/tsconfig.json', {
+//     typescript: require('typescript')
+// });
+//
+// // compile-ts
+// gulp.task('compile-ts', function () {
+//     var tsResult = gulp.src('src/**/*')
+//         .pipe(ts(tsProject));
+//
+//     return tsResult.js.pipe(gulp.dest('build'));
+// });
